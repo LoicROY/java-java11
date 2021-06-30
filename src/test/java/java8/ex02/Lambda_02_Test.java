be631cf701a0991c7be57e7bad7e4db906be7c5a
Loïc ROY <loic.roy35@orange.fr>
2021-06-30 14:55:15 +0200

    fin de tp

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -37,7 +38,7 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, p -> new Account(p, 100));
 
         assert result.size() == personList.size();
         for (Account account : result) {
